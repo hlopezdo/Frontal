a4522f3de8a530b895749303ef5584353fe8e65c
Héctor López Domínguez <hectorlopez81@gmail.com>
2019-03-23 21:50:14 +0100

    Prueba primera ventana, explorador de archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DesktopComponent } from './desktop/desktop.component';
 import { TaskBarComponent } from './task-bar/task-bar.component';
+import { FileExplorerComponent } from './file-explorer/file-explorer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DesktopComponent,
-    TaskBarComponent
+    TaskBarComponent,
+    FileExplorerComponent
   ],
   imports: [
     BrowserModule
